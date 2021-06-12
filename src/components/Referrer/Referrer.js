e07280b356219658722c5c3a79bea072dfac50ff
sujith <sujithsuresh246@gmail.com>
2021-06-12 22:57:58 +0530

    referrer table added

---
+++ src/components/Referrer/Referrer.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { ReferrerData } from "../../config/ReferrerData";
+import './Referrer.scss';
+
+function Referrer() {
+  return (
+    <div className="Referrer">
+    <h3>Referrer</h3>
+      <table className="Table">
+        <tr className="Table__Header">
+          <th>Location</th>
+          <th>Views</th>
+          <th>Sales</th>
+          <th>Conversion</th>
+          <th>Total</th>
+        </tr>
+        {ReferrerData.map((data, index) => (
+          <tr key={index}>
+            <td>{data.location}</td>
+            <td>{data.views}</td>
+            <td>{data.sales}</td>
+            <td>{data.conversion}</td>
+            <td>{data.total}</td>
+          </tr>
+        ))}
+      </table>
+    </div>
+  );
+}
+
+export default Referrer;
