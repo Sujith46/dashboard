5700c788792a621ddc02b368bf67e3a9c1dc7a5a
sujith <sujithsuresh246@gmail.com>
2021-06-12 22:56:38 +0530

    data for components

---
+++ src/config/ReferrerData.js
@@ -0,0 +1,37 @@
+export const ReferrerData = [
+  {
+    location: "google.com",
+    views: "3746",
+    sales: "752",
+    conversion: "43%",
+    total: "$19,292",
+  },
+  {
+    location: "facebook.com",
+    views: "4646",
+    sales: "798",
+    conversion: "53%",
+    total: "$24,292",
+  },
+  {
+    location: "twitter.com",
+    views: "3246",
+    sales: "722",
+    conversion: "45%",
+    total: "$17,292",
+  },
+  {
+    location: "linkedin.com",
+    views: "4746",
+    sales: "852",
+    conversion: "63%",
+    total: "$29,292",
+  },
+  {
+    location: "medium.com",
+    views: "2746",
+    sales: "712",
+    conversion: "48%",
+    total: "$29,292",
+  },
+];
---
+++ src/config/SalesData.js
@@ -0,0 +1,18 @@
+export const Sales = [
+  {
+    name: "France",
+    value: "4260 Sales",
+  },
+  {
+    name: "France",
+    value: "4260 Sales",
+  },
+  {
+    name: "Italy",
+    value: "Italy",
+  },
+  {
+    name: "Italy",
+    value: "Italy",
+  },
+];
